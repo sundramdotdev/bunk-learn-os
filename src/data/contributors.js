7f3d33b2f9bc787b2ed07ebd53a192e91db4f929
sundramdotdev <sundram.devv@gmail.com>
2026-04-21 21:13:31 +0530

    updating contributors data

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -3,11 +3,25 @@
 
 const contributors = [
     {
-        name: "Sundram",
+        name: "Sundram Gupta",
         role: "Project Lead & Core Developer",
         contributions: 14,
         github: "https://github.com/sundramdotdev",
-        linkedin: "#",
+        linkedin: "https://www.linkedin.com/in/sundaramdotdev",
+    },
+    {
+        name: "Praveen Kumar Singh",
+        role: "Lead Contributor",
+        contributions: 4,
+        github: "https://github.com/webdevpraveen",
+        linkedin: "https://www.linkedin.com/in/praveenksingh-/",
+    },
+    {
+        name: "Rishabh Dhar Dwivedi",
+        role: "Contributor",
+        contributions: 2,
+        github: "https://github.com/rishabh-dwivedi22",
+        linkedin: "https://www.linkedin.com/in/rishabh--dhar-dwivedi22/",
     },
 ];
 
